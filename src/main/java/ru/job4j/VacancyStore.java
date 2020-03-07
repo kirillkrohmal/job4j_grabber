a9eb65513ef93efadcfab77badd219e57d228167
kirillkrohmal <krohmal_kirill@mail.ru>
2020-03-07 18:17:53 +0300

    Парсер вакансий на sql.ru[#80118]

---
+++ src/main/java/ru/job4j/VacancyStore.java
@@ -0,0 +1,22 @@
+package ru.job4j;
+
+public class VacancyStore {
+    Vacancy save;
+    Vacancy find;
+
+    public Vacancy getSave() {
+        return save;
+    }
+
+    public void setSave(Vacancy save) {
+        this.save = save;
+    }
+
+    public Vacancy getFind() {
+        return find;
+    }
+
+    public void setFind(Vacancy find) {
+        this.find = find;
+    }
+}
